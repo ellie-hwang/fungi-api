78de769baeeb96319a82b3cb52ca1dd381bab7c1
Eleonora Hwang <eleonorahwang@gmail.com>
2022-05-23 23:05:09 -0700

    Completed database-setup

---
+++ data.sql
@@ -0,0 +1,6 @@
+insert into "fungi" ("name", "family", "species", "edibility", "season", "imageUrl")
+values  ('common chanterelle', 'cantharellaceae', 'cantharellus cibarius', 'edible', 'summer-winter', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/2007-07-14_Cantharellus_cibarius.jpg/1200px-2007-07-14_Cantharellus_cibarius.jpg'),
+        ('jack o lantern', 'paxillaceae', 'omphalotus olearius', 'poisonous', 'summer-autumn', 'https://learnyourland.com/wp-content/uploads/2018/08/cantharelluslateritius2018-2-e1535482499586-1024x669.jpg'),
+        ('false chanterelle', 'paxillaceae', 'hygrophoropsis aurantiaca', 'indedible', 'summer-winer', 'https://i0.wp.com/www.fungusfactfriday.com/wp-content/uploads/2017/08/Hygrophoropsis-aurantiaca-1-1024x576.jpg?resize=1024%2C576&ssl=1'),
+        ('panther amanita', 'amanitaceae', 'amanita pantherina', 'poisonous', 'summer-autumn', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Amanita_pantherina_2013_G1.jpg/1200px-Amanita_pantherina_2013_G1.jpg'),
+        ('shaggy-mane inky cap', 'coprinaceae', 'coprinus comatus', 'edible', 'autumn', 'https://images.saymedia-content.com/.image/t_share/MTc0Mjk3NTMyMjY2OTgxMjQ0/inky-caps-unusual-mushrooms-and-their-health-effects.jpg');
