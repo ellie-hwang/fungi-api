501b14c3a5fb869696c48265854af618243a79b3
Eleonora Hwang <eleonorahwang@gmail.com>
2022-05-24 13:29:32 -0700

    Completed post-route

--- index.js
+++ index.js
@@ -25,6 +25,23 @@ app.get("/api/fungi", (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.post("/api/fungi", (req, res, next) => {
+  const { name, family, species, edibility, season, imageUrl } = req.body;
+  if (!name || !family || !species || !edibility || !season || !imageUrl) {
+    throw new ClientError(400, "name, family, species, edibility, season, and imageUrl are required fields!");
+  }
+  const sql = `insert into "fungi" ("name", "family", "species", "edibility", "season", "imageUrl")
+              values ($1, $2, $3, $4, $5, $6)
+              returning *;`
+  const params = [name, family, species, edibility, season, imageUrl];
+  db.query(sql, params)
+    .then(result => {
+      const [newFungi] = result.rows;
+      res.status(201).json(newFungi);
+    })
+    .catch(err => next(err));
+})
+
 app.use(errorMiddleware);
 
 app.listen(3000, () => {
